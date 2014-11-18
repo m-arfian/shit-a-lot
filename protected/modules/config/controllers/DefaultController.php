fdccc7ca4f21d25d8ceb1c8282a3032839c135d4
Muchammad Arfian <muchammad.arfian@gmail.com>
2014-11-18 10:00:23 +0700

    time range

--- protected/modules/config/controllers/DefaultController.php
+++ protected/modules/config/controllers/DefaultController.php
@@ -1,9 +1,20 @@
 <?php
 
-class DefaultController extends Controller
-{
-	public function actionIndex()
-	{
-		$this->render('index');
-	}
+class DefaultController extends Controller {
+
+    public function actionIndex() {
+        $this->redirect(array('format'));
+    }
+    
+    public function actionFormat() {
+        $model = new DaterangeConfig();
+        if (isset($_POST['DaterangeConfig'])) {
+            $model->attributes = $_POST['DaterangeConfig'];
+            if ($model->validate()) {
+                
+            }
+        }
+        $this->render('contact', array('model' => $model));
+    }
+
 }
---
+++ protected/modules/config/controllers/TimeRangeController.php
@@ -0,0 +1,11 @@
+<?php
+
+class TimeRangeController extends Controller {
+
+    public function actionIndex() {
+        
+        
+        $this->render('format');
+    }
+
+}
